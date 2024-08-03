84fc1aaf673134576ddc0a1bfb0362391a70ed5b
Timur Sefergil <140344749+CTimurSefergil@users.noreply.github.com>
2024-08-04 01:03:44 +0300

    Update map.rs

--- src/game/spawn/map.rs
+++ src/game/spawn/map.rs
@@ -11,19 +11,11 @@ pub(super) fn plugin(app: &mut App) {
 pub struct SpawnMap;
 
 fn spawn_map(_trigger: Trigger<SpawnMap>, mut commands: Commands) {
-    /* Create the ground. */
-    commands
-        .spawn((
-            Collider::cuboid(100.0, 0.1, 100.0),
-            StateScoped(Screen::Playing),
-        ))
-        .insert(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
-
     /* Create the bouncing ball. */
+
     commands
         .spawn((RigidBody::Dynamic, StateScoped(Screen::Playing)))
         .insert(Collider::ball(2.0))
         .insert(Restitution::coefficient(0.7))
         .insert(TransformBundle::from(Transform::from_xyz(0.0, 16.0, -10.0)));
 }
-//
