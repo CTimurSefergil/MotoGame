84884d6540a2a1acf6855cd9a26f1d2760ecd37a
Timur Sefergil <sefergil29@gmail.com>
2024-08-09 09:46:03 +0300

    Tamam

--- src/game/map_change.rs
+++ src/game/map_change.rs
@@ -19,13 +19,13 @@ pub(super) fn plugin(app: &mut App) {
     );
 }
 
-const GRID_X: usize = 40;
-const GRID_Z: usize = 40;
+const BIR_SIRADAKI_KARE_SAYISI: usize = 40;
 
-const TILE_W: usize = 8;
-const TILE_H: usize = 8;
+const KARE_KENAR_UZUNLUGU: usize = 8;
 
-const TIME: Duration = Duration::from_millis(500);
+const TIME: Duration = Duration::from_millis(2000);
+
+const PATH: Vec<&'static str> = Vec::new();
 
 #[derive(Component, Debug, Clone, Copy, PartialEq, Default, Reflect)]
 pub struct Map {
@@ -39,33 +39,31 @@ fn create_tile_map(mut commands: Commands, player: Query<&Transform, With<Player
         return;
     };
 
-    for x in 0..GRID_X {
-        for z in 0..GRID_Z {
+    for x in 1..BIR_SIRADAKI_KARE_SAYISI {
+        for z in 1..BIR_SIRADAKI_KARE_SAYISI {
             grids.insert((x, z));
         }
     }
 
     for (en, (x, z)) in grids.iter().enumerate() {
         let (world_x, world_z) = grid_to_world(*x as f32, *z as f32);
-
         commands.spawn((
-            Collider::cuboid(TILE_W as f32 / 2.0, 0.0, TILE_H as f32 / 2.0),
+            Collider::cuboid(
+                KARE_KENAR_UZUNLUGU as f32 / 2.0,
+                0.0,
+                KARE_KENAR_UZUNLUGU as f32 / 2.0,
+            ),
             StateScoped(Screen::Playing),
             TransformBundle::from(Transform::from_xyz(
-                world_x
-                    + GRID_X as f32
-                    + (((player.translation.x.round()) / TILE_W as f32).round() * TILE_W as f32)
-                    - ((GRID_X as f32 + 2.0) / 2.0) * TILE_W as f32,
+                world_x - (KARE_KENAR_UZUNLUGU as f32 * BIR_SIRADAKI_KARE_SAYISI as f32 / 2.0)
+                    + (((player.translation.x.round()) / KARE_KENAR_UZUNLUGU as f32).round()
+                        * KARE_KENAR_UZUNLUGU as f32),
                 0.0,
-                world_z
-                    + GRID_Z as f32
-                    + (((player.translation.z.round()) / TILE_H as f32).round() * TILE_H as f32)
-                    - ((GRID_Z as f32 + 2.0) / 2.0) * TILE_H as f32,
+                world_z - (KARE_KENAR_UZUNLUGU as f32 * BIR_SIRADAKI_KARE_SAYISI as f32 / 2.0)
+                    + (((player.translation.z.round()) / KARE_KENAR_UZUNLUGU as f32).round()
+                        * KARE_KENAR_UZUNLUGU as f32),
             )),
-            Map {
-                //id: Vec3::new(world_x / 2.0, 0.0, world_z / 2.0),
-                id: en as i32,
-            },
+            Map { id: en as i32 },
         ));
     }
 }
@@ -98,8 +96,8 @@ fn update_tiles(
         if *last_sfx + TIME < now {
             *last_sfx = now;
 
-            for x in 0..GRID_X {
-                for z in 0..GRID_Z {
+            for x in 1..BIR_SIRADAKI_KARE_SAYISI {
+                for z in 1..BIR_SIRADAKI_KARE_SAYISI {
                     grids.insert((x, z));
                 }
             }
@@ -108,27 +106,27 @@ fn update_tiles(
                 let (world_x, world_z) = grid_to_world(*x as f32, *z as f32);
 
                 let (location_x, location_y, location_z) = (
-                    world_x
-                        + GRID_X as f32
-                        + (((player.translation.x.round()) / TILE_W as f32).round()
-                            * TILE_W as f32)
-                        - ((GRID_X as f32 + 2.0) / 2.0) * TILE_W as f32,
+                    world_x - (KARE_KENAR_UZUNLUGU as f32 * BIR_SIRADAKI_KARE_SAYISI as f32 / 2.0)
+                        + (((player.translation.x.round()) / KARE_KENAR_UZUNLUGU as f32).round()
+                            * KARE_KENAR_UZUNLUGU as f32),
                     0.0,
-                    world_z
-                        + GRID_Z as f32
-                        + (((player.translation.z.round()) / TILE_H as f32).round()
-                            * TILE_H as f32)
-                        - ((GRID_Z as f32 + 2.0) / 2.0) * TILE_H as f32,
+                    world_z - (KARE_KENAR_UZUNLUGU as f32 * BIR_SIRADAKI_KARE_SAYISI as f32 / 2.0)
+                        + (((player.translation.z.round()) / KARE_KENAR_UZUNLUGU as f32).round()
+                            * KARE_KENAR_UZUNLUGU as f32),
                 );
 
                 if player.translation.distance(Vec3 {
-                    x: location_x - TILE_H as f32 * 2.0,
+                    x: location_x,
                     y: location_y,
-                    z: location_z - TILE_H as f32 * 2.0,
-                }) > 12.0 * TILE_H as f32
+                    z: location_z,
+                }) > (BIR_SIRADAKI_KARE_SAYISI as f32 / 4.0) * KARE_KENAR_UZUNLUGU as f32
                 {
                     commands.spawn((
-                        Collider::cuboid(TILE_W as f32 / 2.0, 0.0, TILE_H as f32 / 2.0),
+                        Collider::cuboid(
+                            KARE_KENAR_UZUNLUGU as f32 / 2.0,
+                            0.0,
+                            KARE_KENAR_UZUNLUGU as f32 / 2.0,
+                        ),
                         StateScoped(Screen::Playing),
                         TransformBundle::from(Transform::from_xyz(
                             location_x, location_y, location_z,
@@ -142,7 +140,9 @@ fn update_tiles(
             }
 
             for (entity, location) in map.iter() {
-                if player.translation.distance(location.translation) > 20.0 * TILE_H as f32 {
+                if player.translation.distance(location.translation)
+                    > BIR_SIRADAKI_KARE_SAYISI as f32 * KARE_KENAR_UZUNLUGU as f32
+                {
                     commands.entity(entity).despawn();
                 }
             }
@@ -151,7 +151,10 @@ fn update_tiles(
 }
 
 fn grid_to_world(x: f32, z: f32) -> (f32, f32) {
-    (x * TILE_W as f32, z * TILE_H as f32)
+    (
+        x * KARE_KENAR_UZUNLUGU as f32,
+        z * KARE_KENAR_UZUNLUGU as f32,
+    )
 }
 
 fn run_if_empty_map(query: Query<(), With<Map>>) -> bool {
